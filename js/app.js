82898f965396d80cf098561025c659d8da558ff5
Darshan Pawar <darshanp40@gmail.com>
2017-09-25 05:50:38 +0530

    v 1.0

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
+(function() {
 /*
  * Create a list that holds all of your cards
  */
+/*
 
+ * /
+ * 
+ * @param {any} array 
+ * @returns 
+ */
 
 /*
  * Display the cards on the page
@@ -25,7 +32,6 @@ function shuffle(array) {
     return array;
 }
 
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -36,3 +42,76 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ var Card = function(icon) {
+    this.HTML = this.createCard(icon);
+    this.icon = icon;
+ }
+
+ Card.prototype.createCard = function(cardIcon) {
+     var cardHTML;
+     cardHTML = ' <li class="card">' +
+                    '<i class="fa ' + cardIcon + '"></i>' +
+                '</li>';
+    return cardHTML;
+ }
+
+function createGameHTML() {
+    var arrCardIcons = ['fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt','fa-cube','fa-leaf','fa-bicycle','fa-bomb'],
+        arrCardObjects = [],
+        HTMLChunk,
+        arrElements,
+        matchedElements = 1,
+        currentOject;
+
+    for (var index = 0; index < arrCardIcons.length * 2; index++) {
+        arrCardObjects[index] = new Card(arrCardIcons[index % arrCardIcons.length]);
+    }
+    arrCardObjects = shuffle(arrCardObjects);
+    
+    HTMLChunk = arrCardObjects.map(function (obj) {
+        return obj.HTML;
+    }).join("");
+
+    document.getElementsByClassName('deck')[0].innerHTML = HTMLChunk;
+    
+    arrElements = document.getElementsByClassName('card');
+    for (var index = 0; index < arrElements.length; index++) {
+        addClickEvent(arrElements[index]);
+    }
+
+    function addClickEvent(obj) {
+        obj.addEventListener('click', function(e) {
+            var firstCard;
+            if(this.className.indexOf('match') > -1) {
+                return;
+            }
+            if(this.className.indexOf('show open') > -1) {
+                return;
+            }
+            if(this.className.indexOf('show open') === -1) {
+                this.className += ' show open';
+            }
+        
+            if(!currentOject) {
+                currentOject = this;
+            } else {
+                setTimeout(function() {}, 2000);
+                firstCard = currentOject.children[0].className.replace("fa ", "");
+                if(firstCard === this.children[0].className.replace("fa ", "")) {
+                    currentOject.className = currentOject.className.replace(" show open", "") + " match"; 
+                    this.className = currentOject.className.replace(" show open", "") + " match";
+                } else {
+                    currentOject.className = currentOject.className.replace(" show open", ""); 
+                    this.className = currentOject.className.replace(" show open", "");
+                }
+                currentOject = "";
+            }
+        });   
+    }
+}
+createGameHTML();
+document.getElementsByClassName('restart')[0].addEventListener('click', function() {
+    createGameHTML();
+});
+})();
